892999d505e93c6b031993f973d3689452b54a8e
Syariffullah <dinnereatc@gmail.com>
2026-06-25 10:20:21 +0700

    init: initialize project with Vite and React setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Main from "./pages/Main";
+
+function App() {
+	return <Main />;
+}
+
+export default App;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import "./main.css";
+
+const Main = () => {
+	return (
+		<main>
+			<h1>Main</h1>
+		</main>
+	);
+};
+
+export default Main;
